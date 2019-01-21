143a66af0218718d02a44c7c2c66423f8c22a4af
Rajiv Giri <rajivgiri513@gmail.com>
2019-01-20 23:49:31 -0500

    Update Registration.java

--- myDesktopApp/Registration.java
+++ myDesktopApp/Registration.java
@@ -251,9 +251,9 @@ public class Registration extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 
 				try{
-					String url = "jdbc:postgresql://ec2-54-227-237-223.compute-1.amazonaws.com:5432/dde5nv2s4avttf?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-					String username = "mdoxaevgiwnvmd";
-					String pwd = "2a871683b1140322e3311f9367702629f7ec7646c0c4f5025698c71090825cff";
+					String url = "your db url";
+					String username = "your user name";
+					String pwd = "your password";
 					Class.forName("org.postgresql.Driver");
 					Connection connection=DriverManager.getConnection(url, username, pwd);
 
